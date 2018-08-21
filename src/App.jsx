ad0742c9bda3d639915d634bf6fcd8100691e150
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-08-21 17:50:00 +0300

    div rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import RandomDiv from './components/RandomDiv'
 
 class App extends React.Component {
+  state = {
+    divs: []
+  }
+
+  componentDidMount() {
+    let divs = []
+    for (let i = 0; i < 50; i++) {
+      divs.push(<RandomDiv />)
+    }
+    this.setState({ divs })
+  }
+
+  display = (i) => {
+    if( i <= this.state.divs.length)
+      setTimeout(() => this.display(++i), 10)
+    else
+      return this.state.divs[i]
+  }
+
   render() {
     return (
       <div className="App">
-
+        {this.display(0)}
       </div>
     )
   }
---
+++ src/components/RandomDiv.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class RandomDiv extends React.Component {
+  render() {
+    return (
+      <div className="RandomDiv">
+        ssa
+      </div>
+    )
+  }
+}
+
+export default RandomDiv
