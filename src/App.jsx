a36566798be08c6d97ac01b583c83592b49a33b8
Edward-Fedoruk <ed.fedorukk@gmail.com>
2018-08-22 13:12:06 +0300

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,43 @@ import RandomDiv from './components/RandomDiv'
 
 class App extends React.Component {
   state = {
-    divs: []
+    divs: [],
+    counter: 0,
+    increment: true,
   }
 
-  componentDidMount() {
-    let divs = []
-    for (let i = 0; i < 50; i++) {
-      divs.push(<RandomDiv />)
-    }
-    this.setState({ divs })
+  randomNumb = () => Math.round(Math.random() * 255)
+
+  interval = () => {
+    setInterval(() => {
+
+      if(this.state.counter === 50) this.setState({ increment: false }) 
+      else if( this.state.counter === 0) this.setState({ increment: true })
+
+      this.setState( state => {
+        if(this.state.increment === true)
+          return {counter: state.counter + 1}
+        else return {counter: state.counter - 1}
+      }, () => {
+        let divs = []
+        for (let i = 0; i < this.state.counter; i++) {
+          divs.push(<RandomDiv rand={this.randomNumb} key={i}/>)
+        }
+        this.setState({ divs })
+      })
+
+    }, 50)
   }
 
-  display = (i) => {
-    if( i <= this.state.divs.length)
-      setTimeout(() => this.display(++i), 10)
-    else
-      return this.state.divs[i]
+  componentDidMount() { 
+    this.interval()
   }
+
 
   render() {
     return (
-      <div className="App">
-        {this.display(0)}
+      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}} className="App">
+        {this.state.divs.map(div => div)}
       </div>
     )
   }
--- src/components/RandomDiv.jsx
+++ src/components/RandomDiv.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 class RandomDiv extends React.Component {
   render() {
     return (
-      <div className="RandomDiv">
-        ssa
+      <div style={{
+        backgroundColor: `rgb(${this.props.rand()}, ${this.props.rand()}, ${this.props.rand()})`,
+        width: '100px',
+        height: '100px',
+        margin: '5px',
+        borderRadius: '5px'
+      }}>
+      {/*  */}
       </div>
     )
   }
